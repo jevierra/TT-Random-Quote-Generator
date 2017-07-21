8a06aaba049ee37b5eccf4c1a065be18dc9cddc2
jeffrey vierra <st.pious@gmail.com>
2017-07-20 15:40:56 -1000

    Added if statements
    I added if statements to check citations & year for null or undefined
    fields.

--- js/quotes.js
+++ js/quotes.js
@@ -1,13 +1,13 @@
 /* Sesame street quotes 
 
-	Create an array of JavaScript objects to hold the data for your quotes. Name the array quotes. The quotes array should be accessible in the global scope. 
+	  {1}  Create an array of JavaScript objects to hold the data for your quotes. Name the array quotes. The quotes array should be accessible in the global scope. 
 		Each quote object in the quotes array should have the following properties:
 	•	A quote property which contains a string: the text of the quote that will be displayed on the page.
 	•	A source property which contains a string identifying the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb.”
 	•	An optional citation property which contains a string identifying where the quote comes from, like a speech or publication. For example, "Famous Anonymous Jokes." If there is no known publication, do not include this property on the object.
 	•	An optional year property which contains a number identifying the date of the quote. For example, 1997. If there is no known date, then do not include this property on the object.
 
-
+					# - (Completed) - # 
 
 */
 
@@ -33,6 +33,7 @@ var quotes = [
 	tag: 'Friendship ',
 	year: 1983
 },
+
 {
 	quote: 'They threw me away, I survived ',
 	source: 'Oscar the Grouch ',
@@ -41,7 +42,7 @@ var quotes = [
 	year:1989
 },
 {
-	quote: 'Where there is life there is hope ',
+	quote:'Where there is life, there is hope',
 	source: 'Grover ',
 	citation: ' Sesame Street ',
 	tag: 'Hope ',
--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-/*  Create a function named getRandomQuote which:
-	 •	selects a random quote object from the quotes array  # - (Completed) - #
-	 •	returns the randomly selected quote object # - (Completed) - #
-*/
+/* -------------------   {2} Create a function named getRandomQuote which: ----------------------------------
+	 •	selects a random quote object from the quotes array
+	 •	returns the randomly selected quote object 
+				
+				# - (Completed) - #
+-------------------------------------------------------------------------------------------------------------*/
 
 function getRandomQuote(){
 var rnum = Math.floor(Math.random() * quotes.length) +1 ;
@@ -17,7 +19,7 @@ return quotes[rnum]
 
 }
 
-/* -----------  Generate an RGB color based on 256 possible colors    (When the quote changes, randomly change the background color of the page.)  ----------  # - (Completed) - #    */
+/* -----------  Generate an RGB color based on 256 possible colors (When the quote changes, randomly change the background color of the page.)  ----------  # - (Completed) - #    */
 
 function getRandomColor() {
 	var color = 'rgb(';
@@ -30,28 +32,55 @@ function getRandomColor() {
 
 
 
-/* ----------  Create a function named printQuote which follows these rules: ------------------------------------------                   
+/* ---------- {3} Create a function named printQuote which follows these rules: ------------------------------------------                   
 	
 	•	printQuote calls the getRandomQuote function and stores the returned quote object in a variable # - (Completed) - #
 	•	printQuote constructs a string containing the different properties of the quote object using the following HTML template: (Completed)
-    •	printQuote doesn't add a for a missing citation or a if the year property is missing 
-	•	printQuote displays the final HTML string to the page. You can use this JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
+    •	printQuote doesn't add a for a missing citation or a if the year property is missing (Completed)
+	•	printQuote displays the final HTML string to the page. You can use this JS snippet to accomplish that: document.getElementById('quote-box').innerHTML (Completed)
 
 	
  ----------------------------------------------------------------------------------------------------------------------   */
-
+var totalQuote;
 
 function printQuote(){
 var co = getRandomQuote();
 
+
+
+
 var quote = '<p class="quote">' + co.quote + '</p>' 
+
+totalQuote += quote;
+
+
 var source = '<span class="source">' + co.source + '</span>' 
-var citation = '<span class="citation">' + co.citation + '</span>' 
-var tag = '<span class="tag">' + co.tag + '</span>' 
-var year = '<span class="year">' + co.year + '</span>' 
+/*var citation = '<span class="citation">' + co.citation + '</span>' */
+
+totalQuote += source;
 
 
-var totalQuote = quote += source += citation += tag += year;
+	if (co.citation != undefined || co.citation !== null){
+	var citation = co.citation;
+	totalQuote += citation;
+	} else{
+	console.log('no citation found'); //not needed but a nice notification
+	}
+
+var tag = '<span class="tag">' + co.tag + '</span>'
+
+totalQuote += tag;
+
+
+if (co.year != undefined || co.year !== null){
+	var year = co.year;
+	totalQuote += year;
+	} else{
+	console.log('no citation found'); //not needed but a nice notification
+	}
+
+
+
 
 getRandomColor();
 
